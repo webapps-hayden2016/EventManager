52e7d13da585c238771c9bcaa005b6d27f2887fe
webapps-hayden2016 <webapps-hayden2016@gmail.com>
2016-12-11 11:44:19 -0500

    Add project files.

---
+++ src/EventManager/Models/ManageViewModels/FactorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventManager.Models.ManageViewModels
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
